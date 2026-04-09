7276296d5160aa5236271f27eebb943d832c8738
thejas <thejassathyesh@gmail.com>
2026-04-09 22:19:55 +0530

    navbar header and basic setup done next it to start with blog.jsx

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Headers from '../components/Header'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Headers/>
+
+    </>
+  )
+}
+
+export default Home
