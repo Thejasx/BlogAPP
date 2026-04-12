ea264adc8e3ed6c54dad847b02ac8088f56913d8
thejas <thejassathyesh@gmail.com>
2026-04-12 14:01:35 +0530

    dasboards admin routes done next is to create list blogs in admin das

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import ListBlog from './pages/admin/ListBlog'
+import AddBlog from './pages/admin/AddBlog'
+import Comments from './pages/admin/Comments'
+import Login from './components/admin/Login'
+
 
 
 const App = () => {
@@ -10,6 +17,14 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/blog/:id' element={<Blog/>}/>
+
+        <Route path='/admin' element={ true ? <Layout/> : <Login/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='listBlog' element={<ListBlog/>}/>
+          <Route path='addBlog' element={<AddBlog/>}/>
+          <Route path='comments' element={<Comments/>}/>
+        </Route>
+
       </Routes>
 
     </div>
